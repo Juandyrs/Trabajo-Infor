ab23c510c70472bc1fec00045499df1c21297e0f
Juandyrs <167967461+Juandyrs@users.noreply.github.com>
2026-05-26 18:59:00 +0200

    Creacion clase casilla con sus respectivos cambios
    falta hacer metodo dibuja

--- Pokemon_etsidi/scr/Casilla.h
+++ Pokemon_etsidi/scr/Casilla.h
@@ -1,5 +1,32 @@
 #pragma once
+#include "Pokemon.h"
+
+enum class TipoCasilla{clara,oscura,neutr,poder,aviso_neutro,aviso_claro,aviso_oscuro};
+
 class Casilla
 {
+	int columna, fila;
+	TipoCasilla tipo;
+	Pokemon* personaje;
+	int cambio;
+
+public:
+
+	Casilla() :fila(0), columna(0), tipo(TipoCasilla::clara), personaje(nullptr), cambio(-1) {}
+
+	void inicializar(int f, int c);
+	Pokemon* obtener_pokemon() const { return personaje; }
+	void colocar_pokemon(Pokemon* p) { personaje = p; }
+	void vaciar() { personaje = nullptr; }
+	bool ocupacion() { return personaje != nullptr; }
+
+	int obtener_fila() const { return fila; }
+	int obtener_columna() const { return columna; }
+	TipoCasilla obtener_tipo() const { return tipo; }
+	int obtener_cambio() const { return cambio; }
+	void avanzar_ciclo();
+
+	void dibujar();
+
 };
 
